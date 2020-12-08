250e4a4337be8041d3cdb45ff6d0ff443b0e02bf
sts10 <sschlinkert@gmail.com>
2020-12-07 21:33:41 -0500

    both further and closer

--- day07/solve.rb
+++ day07/solve.rb
@@ -1,18 +1,42 @@
-def what_colors_can_contain_this_color(color, a_big_hash)
-  all_colors = []
-  if a_big_hash[color]
-    first_order_colors = a_big_hash[color]
-    puts "First order colors found: " + first_order_colors.to_s
-  else
-    return []
+# def what_colors_can_contain_this_color(color, a_big_hash)
+#   all_colors = []
+#   if a_big_hash[color]
+#     first_order_colors = a_big_hash[color]
+#     puts "First order colors found: " + first_order_colors.to_s
+#   else
+#     return []
+#   end
+#   all_colors << first_order_colors
+#   first_order_colors.each do |this_color|
+#     # second_order_colors = what_colors_can_contain_this_color(this_color, a_big_hash)
+#     if what_colors_can_contain_this_color(this_color, a_big_hash).include?("no other")
+#       return all_colors
+#     else
+#       all_colors << what_colors_can_contain_this_color(this_color, a_big_hash)
+#       puts "At this point, all_colors is " + all_colors.to_s
+#     end
+#   end
+# end
+
+def what_colors_can_be_contained_by_this_color(color, a_big_hash, carried_over_all_colors = [])
+  # all_colors = []
+  first_order_colors = []
+
+  a_big_hash.each do |k, v|
+    if v.include?(color)
+      first_order_colors << k
+    end
   end
-  all_colors << first_order_colors
+  puts "for" + color + ": first order colors found: " + first_order_colors.to_s
+
+  carried_over_all_colors << first_order_colors
   first_order_colors.each do |this_color|
     # second_order_colors = what_colors_can_contain_this_color(this_color, a_big_hash)
-    if what_colors_can_contain_this_color(this_color, a_big_hash).include?("no other")
-      return all_colors
+    if what_colors_can_be_contained_by_this_color(this_color, a_big_hash, carried_over_all_colors).include?("no other")
+      return carried_over_all_colors
     else
-      all_colors << what_colors_can_contain_this_color(this_color, a_big_hash)
+      carried_over_all_colors << what_colors_can_be_contained_by_this_color(this_color, a_big_hash, carried_over_all_colors)
+      puts "At this point, carried_over_all_colors is " + carried_over_all_colors.to_s
     end
   end
 end
@@ -38,7 +62,7 @@ end
 
 color_to_find = "shiny gold"
 # ans_colors = self.what_colors_can_contain_this_color("shiny gold", big_hash)
-ans_colors = what_colors_can_contain_this_color(color_to_find, big_hash)
+ans_colors = what_colors_can_be_contained_by_this_color(color_to_find, big_hash)
 puts "ans_colors is " + ans_colors.to_s
 ans = ans_colors.length()
 puts "Part 1 ans is " + ans.to_s
