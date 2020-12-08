308abc259aea7b90d95f28774d6cadeb12f588bf
sts10 <sschlinkert@gmail.com>
2020-12-07 21:14:12 -0500

    so close

--- day07/solve.rb
+++ day07/solve.rb
@@ -1,22 +1,27 @@
 def what_colors_can_contain_this_color(color, a_big_hash)
   all_colors = []
-  arr = a_big_hash[color]
-  all_colors << arr
-  arr.each do |this_color|
-    if what_colors_can_contain_this_color(this_color).include?("no other")
+  if a_big_hash[color]
+    first_order_colors = a_big_hash[color]
+    puts "First order colors found: " + first_order_colors.to_s
+  else
+    return []
+  end
+  all_colors << first_order_colors
+  first_order_colors.each do |this_color|
+    # second_order_colors = what_colors_can_contain_this_color(this_color, a_big_hash)
+    if what_colors_can_contain_this_color(this_color, a_big_hash).include?("no other")
       return all_colors
     else
-      all_colors << what_colors_can_contain_this_color(this_color)
+      all_colors << what_colors_can_contain_this_color(this_color, a_big_hash)
     end
   end
 end
 
 rules = []
-File.open("input").each_line do |line|
+File.open("test_input").each_line do |line|
   rules << line
 end
 
-# big_hash = {}
 big_hash = Hash.new
 
 rules.each do |rule|
@@ -31,7 +36,9 @@ rules.each do |rule|
   big_hash[key] = values
 end
 
+color_to_find = "shiny gold"
 # ans_colors = self.what_colors_can_contain_this_color("shiny gold", big_hash)
-ans_colors = what_colors_can_contain_this_color("shiny gold", big_hash)
+ans_colors = what_colors_can_contain_this_color(color_to_find, big_hash)
+puts "ans_colors is " + ans_colors.to_s
 ans = ans_colors.length()
 puts "Part 1 ans is " + ans.to_s
